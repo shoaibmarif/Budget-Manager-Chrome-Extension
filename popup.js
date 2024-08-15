75c860e5c171a05652105dfd744fc16890c8afbc
shoaibDev97 <shoaibmuhammadarif@gmail.com>
2024-08-15 14:24:00 +0500

    added budget funtionality

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 $(function(){
-    $('#name').keyup(function(){
-        $('#greet').text("Hello "+ $('#name').val())
+    chrome.storage.sync.get("total",function(budget){
+        $("#total").text(budget.total)
     })
-})
+    $("#spendAmount").click(function(){
+        chrome.storage.sync.get("total",function(budget){
+            let newTotal = 0;
+            if(budget.total){
+                newTotal += parseInt(budget.total);
+            }
+
+            let amount = $("#amount").val();
+            if(amount){
+                newTotal += parseInt(amount); 
+            }
+
+            chrome.storage.sync.set({"total":newTotal});
+
+            $("#total").text(newTotal);
+            $("#amount").val("");
+        });
+    });
+});
